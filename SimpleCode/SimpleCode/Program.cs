78a6ec43c3336e097ef6c0f41489af808aeb343f
Max Pijevskiy <52789371+P1J4MK4s@users.noreply.github.com>
2020-04-15 12:29:12 +0300

    Изменения условий и синтаксиса
    Пересмотрел полностью условия отбора, написал более логически правильный вариант, и исправил некоторые моменты в синтаксисе

--- SimpleCode/SimpleCode/Program.cs
+++ SimpleCode/SimpleCode/Program.cs
@@ -8,26 +8,26 @@ namespace SimpleCode
 {
     class Solution
     {
-        public static int[] Solutions(string[] A, string[] B)
+        public static int[] solutions(string[] A, string[] B)
         {
             int[] result = new int[A.Length];
             int resIndex = 0;
-            bool flagAllRight = true;
-            for (int i = 0; i < A.Length; i++){
-                for(int j = 0; j < B.Length; j++){
-                    if (A[i].Contains(B[j]) == false)
+            bool flagAllRight;
+            for (int i = 0; i < A.Length; i++)
+            {
+                flagAllRight = true;
+                for (int j = 0; j < B.Length; j++)
+                {
+                    if (A[i].EndsWith($".{B[j]}") || A[i].Equals(B[j]))
                     {
-                        flagAllRight = true;
-                    }
-                    else { 
                         flagAllRight = false;
-                        break;
                     }
                 }
-                if (flagAllRight) { 
-                    result[resIndex] = i;
-                    resIndex++;
-                }
+                    if (flagAllRight)
+                    {
+                        result[resIndex] = i;
+                        resIndex++;
+                    }
             }
             Array.Resize(ref result, resIndex);
             return result;
@@ -36,8 +36,9 @@ namespace SimpleCode
         {
             string[] A = new string[] { "unlock.microvirus.md", "visitwar.com","visitwar.de","fruonline.co.uk","australia.open.com","credit.card.us" };
             string[] B = new string[] { "microvirus.md","visitwar.de","piratebay.co.uk","list.stolen.credit.card.us"};
-            int[] a = Solutions(A,B);
-            for(int i = 0; i < a.Length; i++) {
+            int[] a = solutions(A,B);
+            for(int i = 0; i < a.Length; i++) 
+            {
                 Console.Write(" {0} ",a[i]); 
             }
         }
